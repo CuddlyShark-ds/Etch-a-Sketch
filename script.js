c1107a2ac5f96383dbef566e74d7f2850075baa7
CuddlyShark <135768450+CuddlyShark-ds@users.noreply.github.com>
2023-08-22 11:40:54 -0400

    use javascript to build html page layout.
    implement function to build color select layout.
    implement function to build coloring grid layout.
    implement function to build eraser options layout.

--- script.js
+++ script.js
@@ -1,18 +1,117 @@
-const p = document.getElementById("parentContainer");
+const parentContainer = document.getElementById("parentContainer");
+const colorSelectionContainer = document.createElement("div");
+const gridContainer = document.createElement("div");
+const optionsContainer = document.createElement("div");
+// New elements
+const titleContainer = document.createElement("h1");
+titleContainer.textContent = "ETCH-A-SKETCH!!";
+// Color selection Elements to be added to the Dom
+const blackInput = document.createElement("input");
+const blackLabel = document.createElement("label");
+const redInput = document.createElement("input");
+const redLabel = document.createElement("label");
+const greenInput = document.createElement("input");
+const greenLabel = document.createElement("label");
+const blueInput = document.createElement("input");
+const blueLabel = document.createElement("label");
+const randomColorInput = document.createElement("input");
+const randomColorLabel = document.createElement("label");
+const eraseInput = document.createElement("input");
+const eraseLabel = document.createElement("label");
+const eraseAllBtn = document.createElement("button");
+
+
+
+function buildPageLayout(){
+    document.body.insertBefore(titleContainer, parentContainer);
+    parentContainer.appendChild(colorSelectionContainer);
+    buildColorSelectLayout();
+    parentContainer.appendChild(gridContainer);
+    buildGrid();
+    parentContainer.appendChild(optionsContainer);
+    buildOptionsLayout();
+}
+
+function buildColorSelectLayout(){
+    colorSelectionContainer.classList.add("colorSelectContainer");
+    // black selection
+    blackInput.type = "radio";
+    blackInput.id = "black";
+    blackInput.name = "colors";
+    blackInput.value = "black";
+    colorSelectionContainer.appendChild(blackInput);
+    blackLabel.for = "black";
+    blackLabel.textContent = "Black";
+    blackLabel.classList.add("colorBlack");
+    colorSelectionContainer.appendChild(blackLabel);
+    colorSelectionContainer.appendChild(document.createElement("br"));
+    // red Selection
+    redInput.type = "radio";
+    redInput.id = "red";
+    redInput.name = "colors";
+    redInput.value = "red";
+    colorSelectionContainer.appendChild(redInput);
+    redLabel.for = "red";
+    redLabel.textContent = "Red";
+    redLabel.classList.add("colorRed");
+    colorSelectionContainer.appendChild(redLabel);
+    colorSelectionContainer.appendChild(document.createElement("br"));
+    // green Selection
+    greenInput.type = "radio";
+    greenInput.id = "green";
+    greenInput.name = "colors";
+    greenInput.value = "green";
+    colorSelectionContainer.appendChild(greenInput);
+    greenLabel.for = "green";
+    greenLabel.textContent = "Green";
+    greenLabel.classList.add("colorGreen");
+    colorSelectionContainer.appendChild(greenLabel);
+    colorSelectionContainer.appendChild(document.createElement("br"));
+    // blue Selection
+    blueInput.type = "radio";
+    blueInput.id = "blue";
+    blueInput.name = "colors";
+    blueInput.value = "blue";
+    colorSelectionContainer.appendChild(blueInput);
+    blueLabel.for = "blue";
+    blueLabel.textContent = "Blue";
+    blueLabel.classList.add("colorBlue");
+    colorSelectionContainer.appendChild(blueLabel);
+    colorSelectionContainer.appendChild(document.createElement("br"));
+}
 
 function buildGrid(){
+    gridContainer.classList.add("gridContainer");
     let square = document.createElement("div");
     square.classList.add("gridSquare");
 
     for(i = 0; i < 256; i++){
         let cloneSquare = square.cloneNode(true);
-        cloneSquare.addEventListener("click", changeNodeColor);
-        p.appendChild(cloneSquare);
+        //cloneSquare.addEventListener("click", changeNodeColor);
+        gridContainer.appendChild(cloneSquare);
     }
 }
 
-function changeNodeColor(){
-    this.style.backgroundColor = "black";
+function buildOptionsLayout(){
+    eraseInput.type = "radio";
+    eraseInput.id = "white";
+    eraseInput.name = "colors";
+    eraseInput.value = "white";
+    optionsContainer.appendChild(eraseInput);
+    eraseLabel.for = "white";
+    eraseLabel.textContent = "Erase single";
+    eraseLabel.classList.add("erase");
+    optionsContainer.appendChild(eraseLabel);
+    optionsContainer.appendChild(document.createElement("br"));
+
+    eraseAllBtn.id = "eraseAllBtn";
+    eraseAllBtn.classList.add("eraseAll");
+    eraseAllBtn.textContent = "erase all";
+    eraseAllBtn.addEventListener('click', () => {
+        console.log("All Gone!!!");
+    });
+    optionsContainer.appendChild(eraseAllBtn);
+    
 }
 
-buildGrid();
+buildPageLayout();
