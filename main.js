01f1cac6d8038c7acfb1fd90cff7e305072bfc7c
CuddlyShark <135768450+CuddlyShark-ds@users.noreply.github.com>
2024-11-29 09:07:04 -0500

    Implemented a fucntion to check for and rest a previously exiting grid

--- main.js
+++ main.js
@@ -2,14 +2,16 @@ function main(){
 
 }
 
-function setGrid($this){
+// When the user presses the select size button, a grid of div's are created.
+function setGrid(){
     let size = document.getElementById("quantity").value;
     let container = document.getElementById("grid-container");
+
+    resetGrid();
 
     for(i = 0; i < size; i++){
         let div = document.createElement("div");
         div.classList.add("row");
-        console.log(i.toString())
         div.setAttribute("id", "row-" + i.toString())
         container.appendChild(div);
         for(j = 0; j < size; j++){
@@ -17,6 +19,17 @@ function setGrid($this){
             box.classList.add("grid-box");
             let row = document.getElementById("row-" + i.toString());
             row.appendChild(box);
+        }
+    }
+}
+
+// removes the old grid if it exists
+function resetGrid(){
+    let container = document.getElementById("grid-container");
+    let rows = document.getElementsByClassName("row");
+    if(rows.length > 0){
+        while(container.firstChild){
+            container.removeChild(container.lastChild);
         }
     }
 }
